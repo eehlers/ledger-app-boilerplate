cf9aacb271cfb374b7273a2bc0981769e1a9c4cb
Eric Ehlers <eric.ehlers@reposit.be>
2020-03-11 13:12:18 +0100

    test script

---
+++ tests/test_foo.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from ledgerblue.comm import getDongle
+from binascii import unhexlify
+
+account_number = "12345"
+
+account = '{:08x}'.format(int(account_number))
+
+# Create APDU message.
+# CLA 0xE0
+# INS 0x02  GET_ADDRESS
+# P1 0x01   USER CONFIRMATION REQUIRED (0x00 otherwise)
+# P2 0x00   UNUSED
+# Ask for confirmation
+# txt = "E0020100" + '{:02x}'.format(len(donglePath) + 1) + '{:02x}'.format( int(len(donglePath) / 4 / 2)) + donglePath
+# No confirmation
+apduMessage = "E0030100" + '{:02x}'.format(len(account) + 1) + account
+apdu = bytearray.fromhex(apduMessage)
+
+print("~~ Ledger Boilerplate ~~")
+print("Request Address")
+
+dongle = getDongle(True)
+result = dongle.exchange(apdu)
+
+#print("Address received: " + result.decode("utf-8"))
